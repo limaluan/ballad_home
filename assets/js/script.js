3346417ab030c7474b92beadead94a0c7c548bb1
limaluan <luanzin1167@gmail.com>
2021-06-30 23:06:15 -0300

    Creates a simple background color switch system
     - Adds color randomizer
     - Adds a background color switch system

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var body = document.querySelector("body");
+var colorTable = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 128, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(128, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 128)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 128, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(128, 0, 255)",
+  "rgb(255, 0, 255)",
+  "rgb(255, 0, 128)",
+  "rgb(255, 0, 64)",
+];
+
+// Randomize a selected color
+var randomizeColor = function () {
+  let random = (Math.random() * 12).toFixed();
+  console.log(`RANDOM DEBUG: ${random}`);
+  return colorTable[random];
+};
+
+// Change the background color
+var colorChanger = function () {
+  let color = randomizeColor();
+
+  while (true) {
+    if (body.style.backgroundColor !== color) {
+      body.style.backgroundColor = color;
+      break;
+    } else {
+      color = randomizeColor();
+    }
+  }
+};
+
+body.style.backgroundColor = randomizeColor();
+setInterval(colorChanger, 3000);
